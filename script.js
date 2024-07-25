7567584a4abc3e994a288cd35354ec0596f99a44
NhatMinh <nguyennhatminh031106@gmail.com>
2024-07-25 10:37:28 +0700

    Practice class

--- script.js
+++ script.js
@@ -1,67 +1,72 @@
-const myLibrary = [];
+class Library {
+    library = [];
 
-function Book (title, author, numOfPages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.read = readStatus;
-}
-
-Book.prototype.toggleRead = function () {
-    this.read = !this.read;
-}
-
-
-function toggleRead(index) {
-    myLibrary[index].toggleRead();
-    displayBooks();
-}
-
-function removeBook(index) {
-    myLibrary.splice(index, 1);
-    displayBooks();
-}
-
-function addBookToLibrary (data) {
-    const title = document.querySelector("#title").value;
-    const author = document.querySelector("#author").value;
-    const numOfPages = document.querySelector("#numpages").value;
-    const read = document.querySelector("#readstatus").checked;
-    const book = new Book(title, author, numOfPages, read);
-    myLibrary.push(book);
-}
-
-function displayBooks() {
-    const table = document.querySelector("table");
-    table.innerHTML = `   
-    <thead>
-        <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Author</th>
-            <th scope="col">Number of pages</th>
-            <th scope="col">Read satus</th>
-            <th></th>
-        </tr>
-    </thead>
-    `; 
-    const body = document.createElement("tbody");
-    for (let i = 0; i < myLibrary.length; i++){
-        const book = myLibrary[i];
-        const row = document.createElement("tr");
-        row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.numOfPages}</td>
-        <td>${book.read ? "Read" : "Not read yet"}</td>
-        <td>
-            <button onclick="toggleRead(${i})">Toggle Read</button>
-            <button onclick="removeBook(${i})">Remove</button>
-        </td>
-        `
-        body.appendChild(row);
+    addBook() {
+        const title = document.querySelector("#title").value;
+        const author = document.querySelector("#author").value;
+        const pages = document.querySelector("#numpages").value;
+        const read = document.querySelector("#readstatus").checked;
+        const book = new Book(title, author, pages, read);
+        this.library.push(book);
     }
-    table.append(body);
+
+    toggleRead(index) {
+        this.library[index].toggleRead();
+        this.displayBooks();
+    }
+    
+    removeBook(index) {
+        this.library.splice(index, 1);
+        this.displayBooks();
+    }
+
+    displayBooks(){
+        const table = document.querySelector("table");
+        table.innerHTML = `   
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Author</th>
+                <th scope="col">Number of pages</th>
+                <th scope="col">Read satus</th>
+                <th></th>
+            </tr>
+        </thead>
+        `; 
+        const body = document.createElement("tbody");
+        for (let i = 0; i < this.library.length; i++){
+            const book = this.library[i];
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td>${book.read ? "Read" : "Not read yet"}</td>
+            <td>
+                <button onclick="myLibrary.toggleRead(${i})">Toggle Read</button>
+                <button onclick="myLibrary.removeBook(${i})">Remove</button>
+            </td>
+            `
+            body.appendChild(row);
+        }
+        table.append(body);
+    }
 }
+
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
+}
+
+const myLibrary = new Library();
 
 const dialog = document.querySelector("dialog");
 const newBook = document.querySelector("dialog + button");
@@ -79,7 +84,7 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    addBookToLibrary();
+    myLibrary.addBook();
     dialog.close();
-    displayBooks();
+    myLibrary.displayBooks()
 });
